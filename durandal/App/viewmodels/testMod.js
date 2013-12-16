ca6a7131bf913cbc2c8cf7464e43bee576a372b0
cptbucky <cpt.bucky@gmail.com>
2013-12-16 14:51:42 +0000

    basic durandal starter project with karma integration

---
+++ durandal/App/viewmodels/testMod.js
@@ -0,0 +1,19 @@
+﻿define(['durandal/app'], function(app) {
+    var model = function () {
+        this.testMe = function(bool) {
+            return bool;
+        };
+    };
+
+    return new model();
+});
+
+//define(function (require) {
+//    var model = function () {
+//        this.testMe = function(bool) {
+//            return bool;
+//        };
+//    };
+
+//    return new model();
+//});
---
+++ durandal/test/test-main.js
@@ -0,0 +1,45 @@
+﻿var tests = [];
+for (var file in window.__karma__.files) {
+    if (/Test\.js$/.test(file)) {
+        tests.push(file);
+    }
+}
+
+requirejs.config({
+    // Karma serves files from '/base'
+    baseUrl: '/base/App',
+
+    paths: {
+        'jquery': '../Scripts/jquery-1.9.1',
+        'knockout': '../Scripts/knockout-2.3.0',
+        'text': '../Scripts/text',
+        'durandal': '../Scripts/durandal',
+        'plugins': '../Scripts/durandal/plugins',
+        'transitions': '../Scripts/durandal/transitions'
+    }, 
+
+    shim: {
+        'durandal': {
+            deps: ['ko', 'jquery'],
+            exports: 'durandal'
+        }
+    }, 
+
+    //shim: {
+    //    'ko': { 'exports': 'knockout' },
+    //},
+    
+    // ask Require.js to load these files (all our tests)
+    deps: tests,
+
+    // start test run, once Require.js is done
+    callback: window.__karma__.start
+});
+
+require(["knockout"], function (ko) {
+    //manually set the global ko property
+    window.ko = ko;
+});
+
+//define('jquery', function () { return jQuery; });
+//define('knockout', ko);
